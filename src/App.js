14e4f068dc3f26010b7a3d516409c7dac8c823c6
yuhs0383 <yuhs0383@naver.com>
2022-06-13 16:28:42 +0900

    로그인 화면 구성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import Header from './Header'
 import Home from './Home';
-import Sidebar from './components/Sidebar'
 import Login from './Login'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -10,7 +9,7 @@ function App() {
     <BrowserRouter>
       <div className="App">
         <Header />
-        <Sidebar width={300} />
+
         <Routes>
           <Route path="/" element={<Home />} />
 
--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './Home.css';
+import Sidebar from './components/Sidebar'
 
 function Home() {
   return (
     <div className="home">
+      <Sidebar width={300} />
       <div className="home-container">
         <img className="home-img" src="https://forward.nhn.com/2020/seoul/hands-on-labs/toastui.chart-dashboard/_images/step08.png"
           alt="" />
