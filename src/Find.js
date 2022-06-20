0227adff6bdb76739997574f6996883b90c0e703
yuhs0383 <yuhs0383@naver.com>
2022-06-20 11:35:55 +0900

    마커 추가

--- src/Find.js
+++ src/Find.js
@@ -3,34 +3,6 @@ import React, { useEffect } from 'react'
 import "./Find.css"
 import Sidebar from "./components/Sidebar";
 
-{/*
-import React, { useEffect } from 'react'
-import "./Find.css"
-
-const Find = () => {
-
-  useEffect(() => {
-    var mapContainer = document.getElementById('map'),
-    mapOption = { 
-        center: new kakao.maps.LatLng(33.450701, 126.570667),
-        level: 3 // 지도의 확대 레벨
-    };
-    var map = new kakao.maps.Map(mapContainer, mapOption); 
-  }, [])
-
-
-  return (
-    <div>
-      <div id="map" style={{ width: "500px", height: "400px" }}></div>
-    </div>
-  )
-}
-
-export default Find;
-*/}
-
-
-
 function Find() {
 
   useEffect(() => {
@@ -39,13 +11,30 @@ function Find() {
         center: new kakao.maps.LatLng(33.450701, 126.570667),
         level: 3 // 지도의 확대 레벨
       };
-    var map = new kakao.maps.Map(mapContainer, mapOption);
+    var map = new kakao.maps.Map(mapContainer, mapOption); //지도 생성
+
+    // 마커가 표시될 위치입니다 
+    var markerPosition = new kakao.maps.LatLng(33.450701, 126.570667);
+
+    // 마커를 생성합니다
+    var marker = new kakao.maps.Marker({
+      position: markerPosition
+    });
+
+    // 마커가 지도 위에 표시되도록 설정합니다
+    marker.setMap(map);
+
+    // 아래 코드는 지도 위의 마커를 제거하는 코드입니다
+    // marker.setMap(null);    
+
   }, [])
+
+
 
   return (
     <div className="find">
       <Sidebar width={300} />
-      <div className="map" id="map" style={{ width: "500px", height: "400px" }}></div>
+      <div className="map" id="map" style={{ width: "auto", height: "400px" }}></div>
     </div>
   );
 }
